316204e3198aff6ab00b4e42165f82071987c754
RayhanLauzzadani <rlauzzadani@gmail.com>
2025-10-16 09:10:07 +0700

    Modifikasi tampilan logo dengan latar belakang lingkaran dan penempatan yang lebih tinggi

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import AboutUs from "../sections/about/AboutUs";  // Menambahkan AboutUs import
 
-export default function About() {
+export default function AboutPage() {
   return (
     <>
+      {/* Spacer agar konten tidak ketutup navbar */}
       <div className="h-16" />
-      <h1>Tentang Kami</h1>
+      
+      <main className="overflow-x-hidden">
+        {/* Section 1: AboutUs untuk carousel dan CV Yudha Pratama */}
+        <AboutUs />  {/* Menambahkan komponen AboutUs yang berisi carousel dan CV */}
+
+        {/* Section 2: AboutClean dengan carousel, stats, dan Partners/Clients */}
+        {/* Note: PartnersClients sudah di-render di dalam AboutClean */}
+      </main>
     </>
   );
 }
